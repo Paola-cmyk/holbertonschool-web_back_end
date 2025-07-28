49af64bda0db9d5307dcade7979538494565883a
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-07-28 14:03:00 -0400

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+"""
+    annotated function that takes two float arguments (a, b),
+    and returns as a float
+"""
+
+
 def add(a: float, b: float) -> float:
+    """ two floating-point numbers """
     return a + b
