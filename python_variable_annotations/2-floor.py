475e88cd732cfa6adfc5e3d094bd76cec18aa67f
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-07-28 14:10:27 -0400

    second

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+   function that takes a float as an argument,
+    and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """ returns the floor of a float """
+    return int(n)
