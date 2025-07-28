0a3eedf74e86c3a1e8f729f0e24589b7b80f09cb
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-07-28 14:15:08 -0400

    3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+   function that takes a float,
+    and returns a string representation of that float
+"""
+
+
+def to_str(n: float) -> str:
+    """ returns a string representation """
+    return str(n)
