2a9771502fcd62d41d4717edd31bb567f6a20a67
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-08-14 11:09:50 -0400

    final cut

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" contains functions for pagination """
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """ returns the index and size of a page """
+    return (((page * page_size) - page_size), (page * page_size))
+
+
+class Server:
+    """ paginate a database of popular baby names. """
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """ cached dataset """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ correct amount of items per page """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        data = Server.dataset(self)
+        pp = index_range(page, page_size)
+        return data[pp[0]:pp[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """ the page information """
+        data = Server.dataset(self)
+        total_pages = len(data) // page_size
+        next_page = None
+        if page < total_pages:
+            next_page = page + 1
+        prev_page = None
+        if page > 1:
+            prev_page = page - 1
+
+        page_info = {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
+        return page_info
---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""deletion-resilient hypermedia pagination"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """paginate a database of popular baby names """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """sorting position, starting at 0 """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ determine pagination index """
+        full_data = self.indexed_dataset()
+        assert index < len(full_data) and index >= 0
+        data = []
+        next_index = index + page_size
+        idx = index
+        for _idx in range(page_size):
+            if not full_data.get(idx):
+                next_index += 1
+                idx += 1
+            data.append(full_data.get(idx))
+            idx += 1
+        idx_dict = {
+            "index": index,
+            "data": data,
+            "next_index": next_index,
+            "page_size": page_size
+        }
+        return idx_dict
